e99176f5836ac75bde3d89e27a78125f65f9151a
Idzi Bartłomiej <idzi.bart@gmail.com>
2023-07-07 12:58:53 +0200

    added some styles

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     displayMessage("🎉 Correct Number!");
     document.querySelector(".number").textContent = secretNumber;
-    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector("body").style.backgroundColor = "#5bdb34";
     document.querySelector(".number").style.width = "30rem";
 
     if (score > highscore) {
